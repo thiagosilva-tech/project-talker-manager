5b06574b27910117573eab5deced1950db13c6e3
ThiagoSilva042 <thiagohenriquedasilva042@gmail.com>
2023-12-06 11:13:47 -0300

    feat: requisito 01

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const fs = require('fs');
+const path = require('path');
 
 const app = express();
 app.use(express.json());
@@ -9,6 +11,14 @@ const PORT = process.env.PORT || '3001';
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
+});
+
+app.get('/talker', (req, res) => {
+  const talkersFilePath = path.join(__dirname, 'talker.json');
+  const talkerData = fs.readFileSync(talkersFilePath, 'utf-8');
+  const talkers = JSON.parse(talkerData);
+
+  res.status(200).json(talkers);
 });
 
 app.listen(PORT, () => {
